1a392e088982687907659178048bd6dd61544707
niv214 <nirav.chhatrola@gmail.com>
2020-05-12 10:15:08 +0530

    Basic security project created

---
+++ security-basics-solution/src/main/java/com/chhatrola/springsecurity/securitybasicssolution/SecurityBasicsSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.chhatrola.springsecurity.securitybasicssolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBasicsSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBasicsSolutionApplication.class, args);
+	}
+
+}
---
+++ security-basics-solution/src/test/java/com/chhatrola/springsecurity/securitybasicssolution/SecurityBasicsSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chhatrola.springsecurity.securitybasicssolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBasicsSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
